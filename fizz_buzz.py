464338eda3669b31d2986693c3ffb6f7f2ab9919
johannakate <jkgriffin@ucdavis.edu>
2015-09-07 13:15:32 -0700

    update to fizzbuzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -6,3 +6,21 @@ def fizz_buzz(n):
 			print "Fizz"
 		if (n % 5) === 0:
 			print "Buzz"
+
+def fizz_buzz(n):
+	for number in xrange(1, n):
+		if (n % 3) and (n % 5) === 0:
+			print "FizzBuzz"
+		if (n % 3) === 0:
+			print "Fizz" 
+		if (n % 5) === 0:
+			print "Buzz"
+
+# if the program can print out the words on separate lines, it is most optimal to follow the code below:
+
+def fizz_buzz(n):
+	for number in xrange(1, n):
+		if (n % 3) === 0:
+			print "Fizz"
+		if (n % 5) === 0:
+			print "Buzz"
